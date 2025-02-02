f5b82de2de0a6751e05c235504b5593f4ec588b2
NeoDev <neoforevershog@gmail.com>
2025-02-03 01:55:34 +0700

    Update prerequisites_download.py

--- rvc/lib/tools/prerequisites_download.py
+++ rvc/lib/tools/prerequisites_download.py
@@ -5,43 +5,7 @@ import requests
 
 url_base = "https://huggingface.co/IAHispano/Applio/resolve/main/Resources"
 
-pretraineds_v1_list = [
-    (
-        "pretrained_v1/",
-        [
-            "D32k.pth",
-            "D40k.pth",
-            "D48k.pth",
-            "G32k.pth",
-            "G40k.pth",
-            "G48k.pth",
-            "f0D32k.pth",
-            "f0D40k.pth",
-            "f0D48k.pth",
-            "f0G32k.pth",
-            "f0G40k.pth",
-            "f0G48k.pth",
-        ],
-    )
-]
-pretraineds_v2_list = [
-    (
-        "pretrained_v2/",
-        [
-            "D32k.pth",
-            "D40k.pth",
-            "D48k.pth",
-            "G32k.pth",
-            "G40k.pth",
-            "G48k.pth",
-            "f0D32k.pth",
-            "f0D40k.pth",
-            "f0D48k.pth",
-            "f0G32k.pth",
-            "f0G40k.pth",
-            "f0G48k.pth",
-        ],
-    )
+
 ]
 models_list = [("predictors/", ["rmvpe.pt", "fcpe.pt"])]
 embedders_list = [("embedders/contentvec/", ["pytorch_model.bin", "config.json"])]
@@ -50,8 +14,6 @@ executables_list = [
 ]
 
 folder_mapping_list = {
-    "pretrained_v1/": "rvc/models/pretraineds/pretrained_v1/",
-    "pretrained_v2/": "rvc/models/pretraineds/pretrained_v2/",
     "embedders/contentvec/": "rvc/models/embedders/contentvec/",
     "predictors/": "rvc/models/predictors/",
     "formant/": "rvc/models/formant/",
@@ -113,32 +75,9 @@ def download_mapping_files(file_mapping_list, global_bar):
             future.result()
 
 
-def split_pretraineds(pretrained_list):
-    f0_list = []
-    non_f0_list = []
-    for folder, files in pretrained_list:
-        f0_files = [f for f in files if f.startswith("f0")]
-        non_f0_files = [f for f in files if not f.startswith("f0")]
-        if f0_files:
-            f0_list.append((folder, f0_files))
-        if non_f0_files:
-            non_f0_list.append((folder, non_f0_files))
-    return f0_list, non_f0_list
-
-
-pretraineds_v1_f0_list, pretraineds_v1_nof0_list = split_pretraineds(
-    pretraineds_v1_list
-)
-pretraineds_v2_f0_list, pretraineds_v2_nof0_list = split_pretraineds(
-    pretraineds_v2_list
-)
 
 
 def calculate_total_size(
-    pretraineds_v1_f0,
-    pretraineds_v1_nof0,
-    pretraineds_v2_f0,
-    pretraineds_v2_nof0,
     models,
     exe,
 ):
@@ -151,18 +90,11 @@ def calculate_total_size(
         total_size += get_file_size_if_missing(embedders_list)
     if exe and os.name == "nt":
         total_size += get_file_size_if_missing(executables_list)
-    total_size += get_file_size_if_missing(pretraineds_v1_f0)
-    total_size += get_file_size_if_missing(pretraineds_v1_nof0)
-    total_size += get_file_size_if_missing(pretraineds_v2_f0)
-    total_size += get_file_size_if_missing(pretraineds_v2_nof0)
+    
     return total_size
 
 
 def prequisites_download_pipeline(
-    pretraineds_v1_f0,
-    pretraineds_v1_nof0,
-    pretraineds_v2_f0,
-    pretraineds_v2_nof0,
     models,
     exe,
 ):
@@ -170,10 +102,7 @@ def prequisites_download_pipeline(
     Manage the download pipeline for different categories of files.
     """
     total_size = calculate_total_size(
-        pretraineds_v1_f0_list if pretraineds_v1_f0 else [],
-        pretraineds_v1_nof0_list if pretraineds_v1_nof0 else [],
-        pretraineds_v2_f0_list if pretraineds_v2_f0 else [],
-        pretraineds_v2_nof0_list if pretraineds_v2_nof0 else [],
+        
         models,
         exe,
     )
@@ -190,13 +119,6 @@ def prequisites_download_pipeline(
                     download_mapping_files(executables_list, global_bar)
                 else:
                     print("No executables needed")
-            if pretraineds_v1_f0:
-                download_mapping_files(pretraineds_v1_f0_list, global_bar)
-            if pretraineds_v1_nof0:
-                download_mapping_files(pretraineds_v1_nof0_list, global_bar)
-            if pretraineds_v2_f0:
-                download_mapping_files(pretraineds_v2_f0_list, global_bar)
-            if pretraineds_v2_nof0:
-                download_mapping_files(pretraineds_v2_nof0_list, global_bar)
+            
     else:
         pass
